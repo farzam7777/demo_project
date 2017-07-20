06b728737770b5d5ad70de13076a0c710b7a466a
farzam <l144211@lhr.nu.edu.pk>
2017-07-20 23:21:16 +0500

    Poster: Model created for posters and paperclip is configured with it.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720165643) do
+ActiveRecord::Schema.define(version: 20170720181850) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 20170720165643) do
     t.integer  "year",        limit: 4
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "posters", force: :cascade do |t|
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|

eb554a43d91afa13a2e57d06ebc305bf7206fd21
farzam <l144211@lhr.nu.edu.pk>
2017-07-28 16:35:28 +0500

    Actor: Actor show page is created for simple users.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
     resources :reviews
   end
   
+  devise_for :users
+  
+  resources :reports
+  resources :actors
+  
   namespace 'admin' do
     root 'movies#index'
     resources :movies do 
@@ -35,10 +40,6 @@ Rails.application.routes.draw do
       get 'edit_pic'
     end
   end
-
-  devise_for :users
-  
-  resources :reports
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

162f913667adcea5a85ce8c623f2e482aa16bd2c
farzam <l144211@lhr.nu.edu.pk>
2017-08-03 17:44:28 +0500

    production.rb modified for email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { :host => 'https://demo-project786.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'l144211@lhr.nu.edu.pk',
+    :password             => 'bismillah786?',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
